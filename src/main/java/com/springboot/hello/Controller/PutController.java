99d633bfe370327e5eb601f3f521721946b863d2
sanghee <wjdtkdgml7352@daum.net>
2022-10-27 15:38:13 +0900

    [추가] swaggerConfiguration

---
+++ src/main/java/com/springboot/hello/Controller/PutController.java
@@ -0,0 +1,21 @@
+package com.springboot.hello.Controller;
+
+import com.springboot.hello.dto.MemberDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/put-api")
+public class PutController {
+
+    @PostMapping(value="/member3")
+    public ResponseEntity<MemberDto>
+    putMember(@RequestBody MemberDto memberDto){
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .body(memberDto);
+    }
+}
