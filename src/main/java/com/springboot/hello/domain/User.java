4560eb9951fae6f643eadb7ac3b92c83a2d50cd8
sanghee <wjdtkdgml7352@daum.net>
2022-10-28 19:20:31 +0900

    [추가] @AllArgsConstructor 어노테이션

--- src/main/java/com/springboot/hello/domain/User.java
+++ src/main/java/com/springboot/hello/domain/User.java
@@ -1,28 +1,17 @@
 package com.springboot.hello.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     private String id;
     private String name;
     private String password;
 
-    public User(String id, String name, String password) {
-        this.id = id;
-        this.name = name;
-        this.password = password;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
 }
