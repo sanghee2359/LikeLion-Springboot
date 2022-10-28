aa8a22bf657ea1c63f1e289d8a8aaadd1693971d
sanghee <wjdtkdgml7352@daum.net>
2022-10-28 14:25:18 +0900

    [추가] UserDao클래스 Component 어노테이션

--- src/main/java/com/springboot/hello/dao/UserDao.java
+++ src/main/java/com/springboot/hello/dao/UserDao.java
@@ -3,20 +3,61 @@ package com.springboot.hello.dao;
 
 import com.springboot.hello.domain.User;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
-
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Map;
+@Component
 public class UserDao {
-    private JdbcTemplate jdbcTemplate;
-    private DataSource dataSource;
+    private final JdbcTemplate jdbcTemplate;
+    private final DataSource dataSource;
 
-    public UserDao(DataSource dataSource) {
+    public UserDao(DataSource dataSource, JdbcTemplate jdbcTemplate) {
         this.dataSource = dataSource;
-        this.jdbcTemplate = new JdbcTemplate(dataSource);
+        this.jdbcTemplate = jdbcTemplate;
+    }
+    public int deleteAll() {
+        return this.jdbcTemplate.update("DELETE FROM users");
     }
     public void add(User user) {
-        this.jdbcTemplate.update("INSERT users(id, name, password) VALUES (?, ?, ?),
+        this.jdbcTemplate.update("INSERT users(id, name, password) VALUES (?, ?, ?)",
                 user.getId(), user.getName(), user.getPassword());
     }
+    public User findById(String id) {
+        Map<String, String> env = System.getenv();
+        Connection c;
+        try {
+            // DB접속 (ex sql workbeanch실행)
+            c = dataSource.getConnection();
 
+            // Query문 작성
+            PreparedStatement pstmt = c.prepareStatement("SELECT * FROM users WHERE id = ?");
+            pstmt.setString(1, id);
+
+            // Query문 실행
+            ResultSet rs = pstmt.executeQuery();
+            User user = null;
+            if (rs.next()) {
+                user = new User(rs.getString("id"), rs.getString("name"),
+                        rs.getString("password"));
+            }
+
+            rs.close();
+            pstmt.close();
+            c.close();
+
+            if (user == null) throw new RuntimeException();
+
+            return user;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
