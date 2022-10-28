c557a24c045eb14256a4ed94b288f077e5309d81
sanghee <wjdtkdgml7352@daum.net>
2022-10-28 14:26:18 +0900

    [추가] deleteAll, findById 메소드

--- src/main/java/com/springboot/hello/dao/UserDao.java
+++ src/main/java/com/springboot/hello/dao/UserDao.java
@@ -3,9 +3,7 @@ package com.springboot.hello.dao;
 
 import com.springboot.hello.domain.User;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
