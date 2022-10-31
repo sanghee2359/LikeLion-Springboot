eff1d591a4a877b6b1aee4044e8acdfdde913c73
sanghee <wjdtkdgml7352@daum.net>
2022-10-31 15:03:44 +0900

    hospital 정보 Insert하는 sql문 작성중

--- src/main/java/com/springboot/hello/dao/HospitalDao.java
+++ src/main/java/com/springboot/hello/dao/HospitalDao.java
@@ -1,4 +1,24 @@
 package com.springboot.hello.dao;
 
+import com.springboot.hello.domain.Hospital;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+
+@Repository
 public class HospitalDao {
+    private final JdbcTemplate jdbcTemplate;
+    private final DataSource dataSource;
+
+    public HospitalDao(DataSource dataSource, JdbcTemplate jdbcTemplate) {
+        this.dataSource = dataSource;
+        this.jdbcTemplate = jdbcTemplate;
+    }
+    public void add(Hospital hospital){
+        this.jdbcTemplate.update("INSERT INTO nation_wide_hospital(id, openServiceName, openLocalGovernmentCode, " +
+                "managementNumber, licenseDate, businessStatus, businessStatusCode, phone, fullAddress, roadNameAddress, " +
+                "hospitalName, businessTypeName, healthcareProviderCount, patientRoomCount, totalNumberOfBeds, totalAreaSize )" +
+                " VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+    }
 }
