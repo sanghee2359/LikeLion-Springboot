e2bbd1a091cf1d83fe45e94e1b75e550fadc2187
sanghee <wjdtkdgml7352@daum.net>
2022-11-02 14:21:28 +0900

    [추가] Service를 생성하고 DB에 값 넣기

--- src/test/java/com/springboot/hello/parser/HospitalParserTest.java
+++ src/test/java/com/springboot/hello/parser/HospitalParserTest.java
@@ -1,6 +1,8 @@
 package com.springboot.hello.parser;
 
+import com.springboot.hello.dao.HospitalDao;
 import com.springboot.hello.domain.Hospital;
+import com.springboot.hello.service.HospitalService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,25 +13,30 @@ import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class HospitalParserTest {
     String line1 = "\"1\",\"의원\",\"01_01_02_P\",\"3620000\",\"PHMA119993620020041100004\",\"19990612\",\"\",\"01\",\"영업/정상\",\"13\",\"영업중\",\"\",\"\",\"\",\"\",\"062-515-2875\",\"\",\"500881\",\"광주광역시 북구 풍향동 565번지 4호 3층\",\"광주광역시 북구 동문대로 24, 3층 (풍향동)\",\"61205\",\"효치과의원\",\"20211115113642\",\"U\",\"2021-11-17 02:40:00.0\",\"치과의원\",\"192630.735112\",\"185314.617632\",\"치과의원\",\"1\",\"0\",\"0\",\"52.29\",\"401\",\"치과\",\"\",\"\",\"\",\"0\",\"0\",\"\",\"\",\"0\",\"\",";
     @Autowired
     ReadLineContext<Hospital> hospitalReadLineContext;
+    @Autowired
+    HospitalService hospitalService;
     @Test
     @DisplayName("10만건 이상 데이터가 파싱되는지")
     void name() throws IOException {
-        String filname = "C:\\Users\\wjdtk\\Downloads\\fulldata_의원.csv";
-        List<Hospital> hospitalList = hospitalReadLineContext.readByLine(filname);
-        assertTrue(hospitalList.size()>10000);
-        assertTrue(hospitalList.size()>100000);
+        String filename = "C:\\Users\\wjdtk\\Downloads\\fulldata_의원.csv";
+        int cnt = hospitalService.insertLargeVolumeHospitalData(filename);
+
+        assertTrue(cnt>10000);
+        assertTrue(cnt>100000);
     }
     @Test
     @DisplayName("csv 1 줄을 Hospital로 잘 만드는지 test")
     void convertToHospital() {
 
-        HospitalParser hp = new HospitalParser();
+        /*HospitalParser hp = new HospitalParser();
         Hospital hospital = hp.parse(line1);
         assertEquals(1, hospital.getId());
         assertEquals("의원", hospital.getOpenServiceName());
@@ -46,6 +53,6 @@ class HospitalParserTest {
         assertEquals(1, hospital.getHealthcareProviderCount());
         assertEquals(0, hospital.getPatientRoomCount());
         assertEquals(0, hospital.getTotalNumberOfBeds());
-        assertEquals(52.29f, hospital.getTotalAreaSize());
+        assertEquals(52.29f, hospital.getTotalAreaSize());*/
     }
 }
